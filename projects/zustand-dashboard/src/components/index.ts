eadb59e1ecc18a1e27ffd73c78647752eb32fbb9
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-01-28 20:26:32 -0600

    feat(projects/zustand-dashboard): Add components and pages for initial zustand dashboard

---
+++ projects/zustand-dashboard/src/components/index.ts
@@ -0,0 +1,3 @@
+export { default as JiraTasks } from './jira/JiraTasks'
+export { default as SideMenu } from './shared/sidemenu/SideMenu'
+export { default as WhiteCard } from './shared/cards/WhiteCard'
