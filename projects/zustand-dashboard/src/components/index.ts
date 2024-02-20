5d3bf5fa8afefa45b84503710a980511c9298e7c
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-19 22:34:00 -0600

    feat(projects/zustand-dashboard): Add use tasks hook

--- projects/zustand-dashboard/src/components/index.ts
+++ projects/zustand-dashboard/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as JiraTasks } from './jira/JiraTasks'
 export { default as SideMenu } from './shared/sidemenu/SideMenu'
 export { default as WhiteCard } from './shared/cards/WhiteCard'
+export { default as RequestInfo } from './shared/request-info/RequestInfo'
