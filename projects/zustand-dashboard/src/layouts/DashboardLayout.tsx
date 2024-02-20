6538aba4f7cfb4a8937073ebad05448d182d6bb8
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-19 22:34:34 -0600

    feat(projects/zustand-dashboard): Add app layouts

--- projects/zustand-dashboard/src/layouts/DashboardLayout.tsx
+++ projects/zustand-dashboard/src/layouts/DashboardLayout.tsx
@@ -1,7 +1,23 @@
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import { SideMenu } from '../components'
+import { useAuthStore } from '../stores'
 
 export default function DashboardLayout() {
+  const authStatus = useAuthStore((state) => state.status)
+  const checkAuthStatus = useAuthStore((state) => state.checkAuthStatus)
+
+  if (authStatus === 'pending') {
+    checkAuthStatus()
+
+    return (
+      <p>Loading...</p>
+    )
+  }
+
+  if (authStatus === 'unauthorized') {
+    return <Navigate to='/auth/login' replace />
+  }
+
   return (
     <div className='bg-slate-200 overflow-y-scroll w-full h-screen antialiased text-slate-900 selection:bg-blue-900 selection:text-white'>
       <div className='flex flex-row relative w-full'>
