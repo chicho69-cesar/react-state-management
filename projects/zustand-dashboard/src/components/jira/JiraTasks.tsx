794ac8724f890f89a9f503ff32fcf1b1cbed8d2c
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-20 22:02:00 -0600

    feat(projects/zustand-dashboard): Add jira page

--- projects/zustand-dashboard/src/components/jira/JiraTasks.tsx
+++ projects/zustand-dashboard/src/components/jira/JiraTasks.tsx
@@ -1,13 +1,38 @@
-import { IoCheckmarkCircleOutline, IoEllipsisHorizontalOutline, IoReorderTwoOutline } from 'react-icons/io5'
+import { IoCheckmarkCircleOutline, IoEllipsisHorizontalOutline } from 'react-icons/io5'
+import classNames from 'classnames'
+
+import { Task, TaskStatus } from '../../interfaces'
+import SingleTask from './SingleTask'
+import { useTasks } from '../../hooks/useTasks'
 
 interface Props {
   title: string
-  value: 'pending' | 'in-progress' | 'done'
+  tasks: Task[]
+  status: TaskStatus
 }
 
-export default function JiraTasks({ title }: Props) {
+export default function JiraTasks({ title, status, tasks }: Props) {
+  const {
+    isDragging,
+    onDragOver,
+    handleAddTask,
+    handleDragOver,
+    handleDragLeave,
+    handleDrop
+  } = useTasks({ status })
+
   return (
-    <div className='!text-black relative flex flex-col rounded-[20px]  bg-white bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px]'>
+    <div
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      onDrop={handleDrop}
+      className={
+        classNames('!text-black border-4  relative flex flex-col rounded-[20px]  bg-white bg-clip-border shadow-3xl shadow-shadow-500  w-full !p-4 3xl:p-![18px]', {
+          'border-blue-500 border-dotted': isDragging,
+          'border-green-500 border-dotted': isDragging && onDragOver,
+        })
+      }
+    >
       {/* Task Header */ }
       <div className='relative flex flex-row justify-between'>
         <div className='flex items-center justify-center'>
@@ -22,36 +47,16 @@ export default function JiraTasks({ title }: Props) {
           </h4>
         </div>
 
-        <button>
+        <button onClick={handleAddTask}>
           <IoEllipsisHorizontalOutline />
         </button>
       </div>
 
       {/* Task Items */ }
       <div className='h-full w-full'>
-        <div className='mt-5 flex items-center justify-between p-2'>
-          <div className='flex items-center justify-center gap-2'>
-            <p className='text-base font-bold text-navy-700'>
-              Tarea número 1
-            </p>
-          </div>
-
-          <span className=' h-6 w-6 text-navy-700 cursor-pointer'>
-            <IoReorderTwoOutline />
-          </span>
-        </div>
-
-        <div className='mt-5 flex items-center justify-between p-2'>
-          <div className='flex items-center justify-center gap-2'>
-            <p className='text-base font-bold text-navy-700'>
-              Tarea número 2
-            </p>
-          </div>
-
-          <span className=' h-6 w-6 text-navy-700 cursor-pointer'>
-            <IoReorderTwoOutline />
-          </span>
-        </div>
+        {tasks.map((task) => (
+          <SingleTask key={task.id} task={task} />
+        ))}
       </div>
     </div>
   )
--- projects/zustand-dashboard/src/pages/02-objects/JiraPage.tsx
+++ projects/zustand-dashboard/src/pages/02-objects/JiraPage.tsx
@@ -1,6 +1,11 @@
 import { JiraTasks } from '../../components'
+import { useTaskStore } from '../../stores'
 
 export default function JiraPage() {
+  const pendingTasks = useTaskStore((state) => state.getTaskByStatus('open'))
+  const inProgressTasks = useTaskStore((state) => state.getTaskByStatus('in-progress'))
+  const doneTasks = useTaskStore((state) => state.getTaskByStatus('done'))
+
   return (
     <>
       <h1>Tareas</h1>
@@ -9,9 +14,9 @@ export default function JiraPage() {
       <hr />
 
       <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
-        <JiraTasks title='Pendientes' value='pending' />
-        <JiraTasks title='Avanzando' value='in-progress' />
-        <JiraTasks title='Terminadas' value='done' />
+        <JiraTasks title='Pendientes' tasks={pendingTasks} status='open' />
+        <JiraTasks title='Avanzando' tasks={inProgressTasks} status='in-progress' />
+        <JiraTasks title='Terminadas' tasks={doneTasks} status='done' />
       </div>
     </>
   )
