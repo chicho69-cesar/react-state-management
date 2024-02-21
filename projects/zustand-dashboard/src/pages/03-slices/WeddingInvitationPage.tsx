989a9362a118b833812d8337d0b70fdb00df073a
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-20 22:03:12 -0600

    feat(projects/zustand-dashboard): Add wedding invitation page

--- projects/zustand-dashboard/src/pages/03-slices/WeddingInvitationPage.tsx
+++ projects/zustand-dashboard/src/pages/03-slices/WeddingInvitationPage.tsx
@@ -1,6 +1,30 @@
+import { type FormEvent } from 'react'
 import { WhiteCard } from '../../components'
+import { useWeddingBoundStore } from '../../stores/wedding'
 
 export default function WeddingInvitationPage() {
+  const firstName = useWeddingBoundStore((state) => state.firstName)
+  const lastName = useWeddingBoundStore((state) => state.lastName)
+  const guestCount = useWeddingBoundStore((state) => state.guestCount)
+  const setFirstName = useWeddingBoundStore((state) => state.setFirstName)
+  const setLastName = useWeddingBoundStore((state) => state.setLastName)
+  const setGuestCount = useWeddingBoundStore((state) => state.setGuestCount)
+
+  const eventYYYYMMDD = useWeddingBoundStore((state) => state.eventYYYYMMDD())
+  const eventHHMM = useWeddingBoundStore((state) => state.eventHHMM())
+  const setEventDate = useWeddingBoundStore((state) => state.setEventDate)
+  const setEventTime = useWeddingBoundStore((state) => state.setEventTime)
+
+  const isConfirmed = useWeddingBoundStore((state) => state.isConfirmed)
+  const setIsConfirmed = useWeddingBoundStore((state) => state.setIsConfirmed)
+
+  const eventDate = useWeddingBoundStore((state) => state.eventDate)
+
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log({ firstName, lastName, guestCount, eventDate, isConfirmed })
+  }
+
   return (
     <>
       <h1>Invitación de Boda</h1>
@@ -10,7 +34,7 @@ export default function WeddingInvitationPage() {
 
       <WhiteCard className='flex items-center justify-center p-12'>
         <div className='mx-auto w-full max-w-[550px]'>
-          <form>
+          <form onSubmit={onSubmit}>
             <div className='-mx-3 flex flex-wrap'>
               <div className='w-full px-3 sm:w-1/2'>
                 <div className='mb-5'>
@@ -25,6 +49,8 @@ export default function WeddingInvitationPage() {
                     name='firstName'
                     id='firstName'
                     placeholder='Primer Nombre'
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
                   />
                 </div>
               </div>
@@ -42,6 +68,8 @@ export default function WeddingInvitationPage() {
                     name='lastName'
                     id='lastName'
                     placeholder='Apellido'
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                   />
                 </div>
               </div>
@@ -61,6 +89,8 @@ export default function WeddingInvitationPage() {
                 placeholder='5'
                 min='0'
                 className='w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md'
+                value={guestCount}
+                onChange={(e) => setGuestCount(+e.target.value)}
               />
             </div>
 
@@ -77,10 +107,12 @@ export default function WeddingInvitationPage() {
                     type='date'
                     name='eventDate'
                     id='eventDate'
+                    value={eventYYYYMMDD}
+                    onChange={(e) => setEventDate(e.target.value)}
                   />
                 </div>
               </div>
-              
+
               <div className='w-full px-3 sm:w-1/2'>
                 <div className='mb-5'>
                   <label
@@ -93,6 +125,8 @@ export default function WeddingInvitationPage() {
                     type='time'
                     name='eventTime'
                     id='eventTime'
+                    value={eventHHMM}
+                    onChange={(e) => setEventTime(e.target.value)}
                   />
                 </div>
               </div>
@@ -102,7 +136,7 @@ export default function WeddingInvitationPage() {
               <label className='mb-3 block text-base font-medium text-[#07074D]'>
                 ¿Tu también vendrás?
               </label>
-              
+
               <div className='flex items-center space-x-6'>
                 <div className='flex items-center'>
                   <input
@@ -110,6 +144,8 @@ export default function WeddingInvitationPage() {
                     name='isComing'
                     id='radioButton1'
                     className='h-5 w-5'
+                    checked={isConfirmed}
+                    onChange={() => setIsConfirmed(true)}
                   />
 
                   <label
@@ -125,8 +161,10 @@ export default function WeddingInvitationPage() {
                     name='isComing'
                     id='radioButton2'
                     className='h-5 w-5'
+                    checked={!isConfirmed}
+                    onChange={() => setIsConfirmed(false)}
                   />
-                  
+
                   <label
                     className='pl-3 text-base font-medium text-[#07074D]'
                   >
