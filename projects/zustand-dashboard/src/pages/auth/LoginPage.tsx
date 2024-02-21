a297c89897f7c51f2763510e08a834f91b11420e
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-20 22:02:19 -0600

    feat(projects/zustand-dashboard): Add login page

--- projects/zustand-dashboard/src/pages/auth/LoginPage.tsx
+++ projects/zustand-dashboard/src/pages/auth/LoginPage.tsx
@@ -1,5 +1,13 @@
+import { type FormEvent } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { useAuthStore } from '../../stores'
+
 export default function LoginPage() {
-  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+  const navigate = useNavigate()
+  const loginUser = useAuthStore((state) => state.loginUser)
+
+  const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     // const { username, password, remember } = event.target as HTMLFormElement
 
@@ -11,9 +19,16 @@ export default function LoginPage() {
 
     console.log(username.value, password.value, remember.checked)
 
-    username.value = ''
-    password.value = ''
-    remember.checked = false
+    try {
+      await loginUser(username.value, password.value)
+      navigate('/dashboard')
+    } catch (error) {
+      console.log('No se puede autenticar')
+    }
+
+    // username.value = ''
+    // password.value = ''
+    // remember.checked = false
   }
 
   return (
