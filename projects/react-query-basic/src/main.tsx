38471208f38e6985a04f9af5be9b8ebd5e50f048
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-21 21:45:08 -0600

    feat(projects/react-query-basic): Add react query provider configuration

--- projects/react-query-basic/src/main.tsx
+++ projects/react-query-basic/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import App from './App.tsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+const queryClient = new QueryClient()
+
+root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
