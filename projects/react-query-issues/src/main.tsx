bae299a6a69dcdee11e15c1695f3b3f8859f1508
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-25 22:28:31 -0600

    feat(projects/react-query-issues): Finish react query issues project

--- projects/react-query-issues/src/main.tsx
+++ projects/react-query-issues/src/main.tsx
@@ -1,10 +1,22 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/styles.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { router } from './issues/index.tsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+const client = new QueryClient()
+
+root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <ReactQueryDevtools />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
