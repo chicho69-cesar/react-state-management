6c6dfd2426b75359f8cd08633cd8621e82eac8eb
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-25 22:27:41 -0600

    feat(projects/react-query-issues): Add issue route

---
+++ projects/react-query-issues/src/issues/views/IssueView.tsx
@@ -0,0 +1,45 @@
+import { Link, Navigate, useParams } from 'react-router-dom'
+
+import { useIssue } from '../hooks'
+import { LoadingIcon } from '../../shared/components/LoadingIcon'
+import { IssueComment } from '../components/IssueComment'
+
+export function IssueView() {
+  const params = useParams()
+  const { id = '0' } = params
+
+  const { issueQuery, commentsQuery } = useIssue(+id)
+
+  if (issueQuery.isLoading) {
+    return <LoadingIcon />
+  }
+
+  if (!issueQuery.data) {
+    return <Navigate to='./issues/list' />
+  }
+
+  return (
+    <div className='row mb-5'>
+      <div className='col-12 mb-3'>
+        <Link to='./issues/list'>Go Back</Link>
+      </div>
+
+      {/* Primer comentario */}
+      <IssueComment issue={issueQuery.data} />
+
+      {/* Comentario de otros */}
+      {commentsQuery.isLoading && (
+        <LoadingIcon />
+      )}
+
+      {commentsQuery.data?.map((issue) => (
+        <IssueComment key={issue.id} issue={issue} />  
+      ))}
+
+      {/* 
+        <IssueComment body={comment2} />
+        <IssueComment body={comment3} /> 
+      */}
+    </div>
+  )
+}
