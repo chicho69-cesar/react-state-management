a7da19e70e9f49b8cae94a2c316842fbdcd11e43
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-02-25 22:27:23 -0600

    feat(projects/react-query-issues): Add list view infinite route

---
+++ projects/react-query-issues/src/issues/views/ListViewInfinite.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+import { State } from '../interfaces'
+import { useIssuesInfinite } from '../hooks'
+import { LabelPicker } from '../components/LabelPicker'
+import { IssueList } from '../components/IssueList'
+import { LoadingIcon } from '../../shared/components/LoadingIcon'
+
+export function ListViewInfinite() {
+  const [selectedLabels, setSelectedLabels] = useState<string[]>([])
+  const [state, setState] = useState<State>()
+
+  const { issuesQuery } = useIssuesInfinite({ state, labels: selectedLabels })
+
+  const onLabelChanged = (labelName: string) => {
+    selectedLabels.includes(labelName)
+      ? setSelectedLabels(selectedLabels.filter((label) => label !== labelName))
+      : setSelectedLabels([...selectedLabels, labelName])
+  }
+
+  return (
+    <div className='row mt-5'>
+      <div className='col-8'>
+        {issuesQuery.isLoading ? (
+          <LoadingIcon />
+        ) : (
+          <IssueList
+            issues={issuesQuery.data?.pages.flat() || []}
+            state={state}
+            onStateChanged={(newState) => setState(newState)}
+          />
+        )}
+
+        <button
+          className='btn btn-outline-primary mt-2'
+          disabled={!issuesQuery.hasNextPage}
+          onClick={() => issuesQuery.fetchNextPage()}
+        >
+          Load more...
+        </button>
+      </div>
+
+      <div className='col-4'>
+        <LabelPicker
+          selectedLabels={selectedLabels}
+          onChange={(labelName) => onLabelChanged(labelName)}
+        />
+      </div>
+    </div>
+  )
+}
