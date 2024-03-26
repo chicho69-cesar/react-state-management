ba5b78877096f14a4070a9b01a441f25b03f85c4
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-25 20:34:26 -0600

    feat(projects/react-query-store): Finish project configurations

--- projects/react-query-store/src/main.tsx
+++ projects/react-query-store/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { NextUIProvider } from '@nextui-org/react'
+import { RouterProvider } from 'react-router-dom'
+
+import { router } from './router/router.tsx'
+import { TanStackProvider } from './plugins/TanStackProvider.tsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
-    <App />
+    <TanStackProvider>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <RouterProvider router={router} />
+        </main>
+      </NextUIProvider>
+    </TanStackProvider>
   </React.StrictMode>,
 )
