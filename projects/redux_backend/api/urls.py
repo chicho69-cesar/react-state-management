6d1c63e5d0c69a124952cc28be2f561d8f6b1066
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-27 18:13:15 -0600

    feat(Root): Finish backend redux project

---
+++ projects/redux_backend/api/urls.py
@@ -0,0 +1,7 @@
+from rest_framework import routers
+from . views import BlogViewSet
+
+router = routers.DefaultRouter()
+router.register(r'blogs', BlogViewSet, 'blogs')
+
+urlpatterns = router.urls
--- projects/redux_backend/api/views.py
+++ projects/redux_backend/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BlogSerializer
+from .models import Blog
 
-# Create your views here.
+class BlogViewSet(viewsets.ModelViewSet):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
