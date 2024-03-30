eabf648c08bea138df00f1a2d30df2bb969495ab
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-30 15:43:39 -0600

    feat(projects/redux-crud): Finish the app project configuration

--- projects/redux-crud/src/App.tsx
+++ projects/redux-crud/src/App.tsx
@@ -1,10 +1,24 @@
-import Main from './modules/01-counter/Main'
+import { Route, Routes } from 'react-router-dom'
+
+import Layout from './components/Layout'
+import AddPost from './pages/AddPost'
+import EditPost from './pages/EditPost'
+import PostList from './pages/PostList'
+import SoloPost from './pages/SoloPost'
 
 function App() {
   return (
-    <>
-      <Main />
-    </>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<PostList />} />
+
+        <Route path='post'>
+          <Route index element={<AddPost />} />
+          <Route path=':postId' element={<SoloPost />} />
+          <Route path='edit/:postId' element={<EditPost />} />
+        </Route>
+      </Route>
+    </Routes>
   )
 }
 
--- projects/redux-crud/src/main.tsx
+++ projects/redux-crud/src/main.tsx
@@ -2,13 +2,22 @@ import './index.css'
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import App from './App.tsx'
+import { store } from './store/store.ts'
 
 const root = ReactDOM.createRoot(document.getElementById('root')!)
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/*' element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
