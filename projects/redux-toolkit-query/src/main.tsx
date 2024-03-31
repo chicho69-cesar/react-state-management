93701c9fcb877e9bc6883f1613f6fb054d48f31e
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-31 13:23:09 -0600

    feat(projects/redux-toolkit-query): Finish project with router and provider config

--- projects/redux-toolkit-query/src/main.tsx
+++ projects/redux-toolkit-query/src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import App from './App.tsx'
+import { store } from './store/store.ts'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
